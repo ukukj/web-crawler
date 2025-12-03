f513cc885c16a69e215bdfef200d6021e96e29d3
yeonhwan <yeonhwan619@gmail.com>
2025-12-03 22:39:38 +0900

    create basic main crawl process

--- main.go
+++ main.go
@@ -2,18 +2,56 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"sync"
 
-	"web-crawler/consts"
+	"web-crawler/crawler"
+	"web-crawler/crawler/parsers/jobkorea"
+	"web-crawler/crawler/parsers/youth_seoul"
+	"web-crawler/models"
 )
 
 func main() {
-	fmt.Println("=== 채용 사이트 ===")
-	for _, site := range consts.Recruits {
-		fmt.Printf("이름: %s, URL: %s\n", site.Name, site.URL)
+	fmt.Println("Starting Crawling Jobs")
+
+	var wg sync.WaitGroup
+	cfg := crawler.NewConfig()
+
+	JobResults := make(chan []models.JobModel, 2)
+	WelfareResults := make(chan []models.WelfareModel, 2)
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		fmt.Println("Begin Crawling:", jobkorea.URL)
+		result, err := crawler.Crawl(jobkorea.URL, cfg, jobkorea.SetupParser)
+		if err != nil {
+			log.Fatal("Crawl Failed", jobkorea.Name, err)
+		}
+		JobResults <- result
+	}()
+
+	go func() {
+		defer wg.Done()
+		fmt.Println("Begin Crawling:", youth_seoul.URL)
+		result, err := crawler.Crawl(youth_seoul.URL, cfg, youth_seoul.SetupParser)
+		if err != nil {
+			log.Fatal("Crawl Failed", youth_seoul.Name, err)
+		}
+		WelfareResults <- result
+	}()
+
+	go func() {
+		wg.Wait()
+		close(JobResults)
+		close(WelfareResults)
+	}()
+
+	allJobs := []models.JobModel{}
+	for jobs := range JobResults {
+		allJobs = append(allJobs, jobs...)
 	}
 
-	fmt.Println("\n=== 복지 사이트 ===")
-	for _, site := range consts.Welfare {
-		fmt.Printf("이름: %s, URL: %s\n", site.Name, site.URL)
-	}
+	fmt.Printf("크롤링 완료. 결과: %d개\n", len(allJobs))
 }
